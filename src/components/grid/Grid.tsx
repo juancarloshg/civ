5c1489dfe3f6dd4c6765ba8304313a804f1da7cd
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-11-24 13:01:14 +0000

    fix tslint

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,61 +1,65 @@
 import * as React from "react";
 import styled from "styled-components";
 
-import {repeat} from "../../utils/utils";
-import {Tile, TileProps} from "./Tile";
-import {TerrainType} from "../terrains/base/TerrainType";
-import {TerrainModifierType} from "../terrains/modifiers/TerrainModifierType";
+import { repeat } from "../../utils/utils";
+import { Tile, TileProps } from "./Tile";
+import { TerrainType } from "../terrains/base/TerrainType";
+import { TerrainModifierType } from "../terrains/modifiers/TerrainModifierType";
 
 interface SquaresRowProps {
-    length: number;
-    row: number;
+  length: number;
+  row: number;
 }
 
 const StyledSquaresRow = styled.span`
   display: flex;
 `;
 
-const size = 50 // TODO get from configuration
-const gameMap = generateMap()
+const size = 50; // TODO get from configuration
+const gameMap = generateMap();
 
 const TilesRow: React.FunctionComponent<SquaresRowProps> = ({
-                                                                length,
-                                                                row
-                                                            }) => (
-    <StyledSquaresRow data-testid="grid-row">
-        {repeat(length, col => (
-            <Tile
-                col={col}
-                row={row}
-                terrain={gameMap[row][col].terrain}
-                terrainModifiers={[TerrainModifierType.PLAIN]}
-                units={[]}
-            />
-        ))}
-    </StyledSquaresRow>
+  length,
+  row
+}) => (
+  <StyledSquaresRow data-testid="grid-row">
+    {repeat(length, col => (
+      <Tile
+        col={col}
+        row={row}
+        terrain={gameMap[row][col].terrain}
+        terrainModifiers={[TerrainModifierType.PLAIN]}
+        units={[]}
+      />
+    ))}
+  </StyledSquaresRow>
 );
 
 export const Grid: React.FunctionComponent = () => (
-    <div data-testid="game-grid">
-        {repeat(size, row => (
-            <TilesRow length={size} row={row}/>
-        ))}
-    </div>
+  <div data-testid="game-grid">
+    {repeat(size, row => (
+      <TilesRow length={size} row={row} />
+    ))}
+  </div>
 );
 
 function generateMap(): TileProps[][] {
-    const map: TileProps[][] = []
-    for (let row = 0; row < size; row++) {
-        map[row] = []
-        for (let col = 0; col < size; col++) {
-            map[row][col] = {
-                row: row,
-                col: col,
-                terrain: TerrainType[Object.keys(TerrainType)[Math.floor(Math.random() * Object.keys(TerrainType).length/2)]] as TerrainType,
-                terrainModifiers: [],
-                units: []
-            }
-        }
+  const map: TileProps[][] = [];
+  for (let row = 0; row < size; row++) {
+    map[row] = [];
+    for (let col = 0; col < size; col++) {
+      map[row][col] = {
+        row,
+        col,
+        terrain: TerrainType[
+          Object.keys(TerrainType)[
+            Math.floor((Math.random() * Object.keys(TerrainType).length) / 2)
+          ]
+        ] as TerrainType,
+        terrainModifiers: [],
+        units: []
+      };
     }
-    return map;
+  }
+  return map;
 }
