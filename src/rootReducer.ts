298a181223b64044b6644b892d06b7921d4a9d0a
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-11-24 23:37:09 +0000

    remove menu hello-world

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,17 +1,14 @@
 import { combineReducers } from 'redux'
 
-import { reducer as menuReducer, MenuState } from './components/menu/menu.reducer'
 import { reducer as configurationReducer, ConfigurationState } from './components/configuration/configuration.reducer'
 import { reducer as gridReducer, GridState } from './components/grid/grid.reducer'
 
 export interface ApplicationState {
-    menu: MenuState
     configuration: ConfigurationState
     grid: GridState
 }
 
 export const rootReducer = combineReducers({
-    menu: menuReducer,
     configuration: configurationReducer,
     grid: gridReducer
 })
--- src/rootSaga.ts
+++ src/rootSaga.ts
@@ -1,7 +1,6 @@
 import { all, fork } from 'redux-saga/effects'
-import { sagas as menuSagas } from './components/menu/menu.sagas'
 import { sagas as gridSagas } from './components/grid/grid.sagas'
 
 export function* rootSaga() {
-    yield all([fork(gridSagas), fork(menuSagas)])
+    yield all([fork(gridSagas)])
 }
