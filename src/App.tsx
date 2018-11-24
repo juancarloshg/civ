67d1d2d355c8a36b5c769025d06c97e6ad190f65
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-11-24 23:30:05 +0000

    add redux logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
 
 import { Router } from './Router'
 import { rootReducer } from './rootReducer'
 import { rootSaga } from './rootSaga'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+
+// tslint:disable-next-line:no-any
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware, logger)))
 sagaMiddleware.run(rootSaga)
 
 export const App: React.FunctionComponent = () => (
