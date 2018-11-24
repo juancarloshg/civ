f1afbab1579ff1e1322972ad5fa991518a98e8ca
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-11-24 17:02:41 +0000

    refactor tile

--- src/components/grid/tile/Tile.tsx
+++ src/components/grid/tile/Tile.tsx
@@ -6,47 +6,37 @@ import { TerrainType } from '../../terrains/base/TerrainType'
 import { TerrainModifierType } from '../../terrains/modifiers/TerrainModifierType'
 import { Unit } from '../../units/Unit'
 
-const StyledTile = styled.span`
-  height: ${squareSize.height};
-  width: ${squareSize.width};
-  display: inline-block;
-  border: 1px solid black;
-  box-sizing: border-box;
-  ${props =>
-      props['data-terrain'] === 'SEA' &&
-      css`
-          background: Aqua;
-      `}
-  ${props =>
-      props['data-terrain'] === 'SNOW' &&
-      css`
-          background: White;
-      `}
-  ${props =>
-      props['data-terrain'] === 'DESERT' &&
-      css`
-          background: Yellow;
-      `}
-  ${props =>
-      props['data-terrain'] === 'DIRT' &&
-      css`
-          background: Olive;
-      `}
-  ${props =>
-      props['data-terrain'] === 'GRASS' &&
-      css`
-          background: Green;
-      `}  
+const getColor = (terrain: TerrainType) => {
+    switch (terrain) {
+        case TerrainType.SEA:
+            return 'Aqua'
+        case TerrainType.SNOW:
+            return 'White'
+        case TerrainType.DESERT:
+            return 'Yellow'
+        case TerrainType.DIRT:
+            return 'Olive'
+        case TerrainType.GRASS:
+            return 'Green'
+    }
+}
+
+const StyledTile = styled.span<TileProps>`
+    height: ${squareSize.height};
+    width: ${squareSize.width};
+    display: inline-block;
+    border: 1px solid black;
+    box-sizing: border-box;
+    ${props =>
+        css`
+            background: ${getColor(props.terrain)};
+        `}
 `
 
 export interface TileProps {
-    row: number
-    col: number
     terrain: TerrainType
     terrainModifiers: TerrainModifierType[]
     units: Unit[]
 }
 
-export const Tile: React.FunctionComponent<TileProps> = ({ row, col, terrain, terrainModifiers }) => (
-    <StyledTile key={`row${row}col${col}`} data-testid="grid-square" data-terrain={terrain} data-terrain-modifier={terrainModifiers} />
-)
+export const Tile: React.FunctionComponent<TileProps> = props => <StyledTile data-testid="grid-square" {...props} />
