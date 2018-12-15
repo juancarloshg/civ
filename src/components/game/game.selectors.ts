d23969e7c5aa11da07ae12b7cebe57b1a95e697d
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-12-15 13:55:33 +0000

    remove empty game selectors

--- src/components/game/game.selectors.ts
+++
@@ -1,6 +0,0 @@
-// import { createSelector, Selector } from 'reselect'
-
-// import { ApplicationState } from 'src/rootReducer'
-// import { GameState } from './game.reducer'
-
-// const getRoot = (state: ApplicationState): GameState => state.game
