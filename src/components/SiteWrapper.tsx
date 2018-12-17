5227d75ca33a56994e8ef04a9f3328ff107be2fe
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-12-17 14:38:33 +0000

    update main layout

---
+++ src/components/SiteWrapper.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const SiteWrapper = styled.div`
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    background: #f5f9e9;
+`
