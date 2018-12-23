b7a9580e2d6827b26fe05c501ab04c61435e4c83
Carlos Hernandez <juancarlosjchg@gmail.com>
2018-12-23 01:19:50 +0000

    add semi-broken zoom in/out functionality

--- src/components/keys/keys.actions.ts
+++ src/components/keys/keys.actions.ts
@@ -1,11 +1,13 @@
 import { createAction, ActionsUnion } from '../../utils/actionHelpers'
 
 export enum ActionTypes {
-    KEYDOWN = '[keys] keydown'
+    KEYDOWN = '[keys] keydown',
+    WHEEL = '[keys] wheel'
 }
 
 export const actions = {
-    keydown: (key: string) => createAction(ActionTypes.KEYDOWN, { key })
+    keydown: (key: string) => createAction(ActionTypes.KEYDOWN, { key }),
+    wheel: (direction: 'in' | 'out') => createAction(ActionTypes.WHEEL, { direction })
 }
 
 export type Actions = ActionsUnion<typeof actions>
